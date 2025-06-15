762ca05e1c36702249f9763795f4bc0d31c5fee9
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-06-15 10:35:44 +0530

    Header Added.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import PostForm from './components/PostForm'
 import PostPreview from './components/PostPreview';
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header';
 
 function App() {
   const [postData, setPostData] = useState({
@@ -12,8 +13,9 @@ function App() {
 
   return (
     <>
-      <h1>VibePost</h1>
-      <hr />
+     
+     <Header/>
+
       <div className="container">
         <PostForm postData={postData} setPostData={setPostData} />
         <PostPreview postData={postData} />
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <h2>VibePost</h2>
+        <hr />
+    </div>
+  )
+}
+
+export default Header

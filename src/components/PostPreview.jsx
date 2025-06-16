ed3df48469aee077ec415d5a26beb877c180d1c6
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-06-16 22:17:11 +0530

    Post UI Design Updated.

--- src/components/PostPreview.jsx
+++ src/components/PostPreview.jsx
@@ -5,21 +5,20 @@ const PostPreview = ({ postData }) => {
   return (
     <div className='pp'>
       <div className='post-preview' id='post-preview'>
-        <div className="quote-con">
-          <h2>{postData.quote}</h2>
-          <hr />
-        </div>
 
-        <div className="author">
+         <div className="author">
           <img src={postData.image} alt="" />
           <p>{postData.name}</p>
+        </div>
+        <div className="quote-con">
+          <h2>{postData.quote}</h2>
         </div>
 
       </div>
 
-<div className="btn-box">
-      <DownloadButton />
-</div>
+      <div className="btn-box">
+        <DownloadButton />
+      </div>
     </div>
 
   )

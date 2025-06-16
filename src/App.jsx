bfd8b1ca4d476ff9eab11cfd14d37571e9f41e54
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-06-16 20:18:29 +0530

    Navbar Created With React Router Dom.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 
 function App() {
   const [postData, setPostData] = useState({
@@ -15,6 +16,7 @@ function App() {
   return (
     <>
       <Header />
+      <Navbar/>
       <div className="container">
         <PostForm postData={postData} setPostData={setPostData} />
         <PostPreview postData={postData} />
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {NavLink} from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <div>
+        <NavLink to={"/"}>Editor</NavLink>
+        <NavLink to={"/preview"}>Preview</NavLink>
+    </div>
+  )
+}
+
+export default Navbar
